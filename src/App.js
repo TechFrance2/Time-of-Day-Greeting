3640ea781a9c9e37925df7b53c32ecc279837e70
Franci Nakamura <franci.nakamura100@gmail.com>
2020-08-29 19:08:04 -0700

    deleted files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+    render () {
+    const date = new Date()
+    const hours = date.getHours ()
+    let timeOfDay
+    const styles ={
+        fontSize: 30
+    }
+
+    if (hours <12){
+        timeOfDay = "morning"
+        styles.color = "#04756F"
+
+    } else if (hours >=12 && hours < 17) {
+        timeOfDay = "afternoon"
+        styles.color = "#8914A3"
+        styles.textAlign = "center"
+
+    } else {
+        timeOfDay= "night"
+        styles.color = "#D9000"
+    }
+
+    
+    return (
+        
+        <h1 style = {styles} > Good {timeOfDay} to you, sir or madam!</h1>
+                
+    )
+  }
 }
 
-export default App;
+export default App  
